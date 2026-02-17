62ba9bb8aeb3ccb0f60122ea50c9b143a28640d8
ogulcannarin <ogulcannarin268@gmail.com>
2026-02-17 09:56:02 +0300

    feat: add department and hobbies to profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Web Tasarımı ve Programlama</h1>
+      <h1>Bölüm: Bilgisayar Programcılığı</h1>
       <h2>LAB-1</h2>
-      <p>Ad Soyad: [Ogulcan narin]</p>
+      <p>Hobiler: Kodlama, Satranç, Müzik</p>
       <p>Öğrenci No: [230542027]</p>
     </div>
   )
