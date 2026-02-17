2a15ea1d83455bff655c1c66494ee5d7ba5c3e44
ogulcannarin <ogulcannarin268@gmail.com>
2026-02-17 09:52:29 +0300

    feat: add personal info to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [Ogulcan narin]</p>
+      <p>Öğrenci No: [230542027]</p>
+    </div>
+  )
+}
+
+export default App
